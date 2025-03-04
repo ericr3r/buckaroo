2b2819958bc9e4c4f9b24cc49fa307b1482a3f40
Eric Rauer <eric@rauer.dev>
2025-03-04 14:54:44 -0800

    Set clipboard register

--- config/settings.nix
+++ config/settings.nix
@@ -6,9 +6,12 @@
     expandtab = true;
   };
 
+  clipboard.register = "unnamedplus";
+
   extraConfigLua = ''
     local servername = vim.api.nvim_get_vvar("servername")
     vim.opt.title = true
     vim.opt.titlestring = string.format("nvim %s -- [%s] ", vim.fn.getcwd(), servername)
+
   '';
 }
